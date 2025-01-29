7ed49e43b54831b311f6d65fb14fba912fab069b
MMAGES2006 <22170@portalcedi.edu.mx>
2025-01-28 21:46:02 -0600

    Fechas

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -8,6 +9,42 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  items: {
+    id: number; name: string; date1: string, date2: string,
+    completed: boolean
+  }[] = [];
+  nextId: number = 1;
+
+
+  constructor(public navCtrl: NavController) {}
+
+  addItem(name: string, date1: string, date2: string): void 
+  {
+    if (name.trim() && date1.trim() && date2.trim()) {
+      this.items.push({
+        id: this.nextId++,
+        name: name.trim(),
+        date1: date1.trim(),
+        date2: date2.trim(),
+        completed: false,
+      });
+    } else {
+      console.error('El nombre y la fecha no pueden estar vacios');
+    }
+  }
+
+  toggleCompleted(id: number): void
+  {
+    const item = this.items.find(item => item.id === id);
+    if(item)
+    {
+      item.completed = !item.completed;
+    }
+  }
+
+  removeItem(id: number): void
+  {
+    this.items = this.items.filter(item => item.id !==id);
+  }
 
 }
